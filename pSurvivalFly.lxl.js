ec00e929acd4e0f4e8739ea6274af5ae2c21c71f
phj233 <2780990934@qq.com>
2022-07-17 16:58:54 +0800

    ~

--- pSurvivalFly.lxl.js
+++ pSurvivalFly.lxl.js
@@ -28,21 +28,38 @@ mc.listen("onServerStarted", () => {
 function p_Gui(player){
     let playerGui = mc.newCustomForm();
     playerGui.setTitle("§4§lpSurvivalFly");
-    playerGui.addInput("您需要的飞行时长(分)："[" ","5"]);
+    playerGui.addInput("您需要的飞行时长(分)：","","5");
     playerGui.addLabel(`§e目前单价"${price}"金币`);
     player.sendForm(playerGui,(player,data) => {
-        if(data[0]!=0){
-            
+        if(data!=0){
+
         }
 
     })
 }
 
+function mgr_Gui(player){
+    let mgrGui = mc.newCustomForm();
+    let onlinePlayer=[];
+    let olPlayerData=mc.getOnlinePlayers();
+    j=olPlayerData.length;
+    for(let i=0;i<j;i++){
+        onlinePlayer.push(olPlayerData[i].realName);
+    }
+    mgrGui.setTitle("§4§lpsFly-mgr");
+    mgrGui.addDropdown("选择一个玩家让他飞行",onlinePlayer);
+    mgrGui.addInput("飞行时间(分)","5");
+    player.sendForm(mgrGui,(player,data)=>{
+        if(data!=null){
 
-function playerFly(){
+        } else{
+
+        }
+    })
+}
+
+function playerFly(pl,time){
 
 }
 
-function mgrFly(){
 
-}
