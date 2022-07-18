c1abee0eb304cf4e51a1ef90df9cbae300a17905
phj233 <2780990934@qq.com>
2022-07-18 14:59:09 +0800

    ~~~

--- pSurvivalFly.lxl.js
+++ pSurvivalFly.lxl.js
@@ -1,5 +1,6 @@
 //LiteLoaderScript Dev Helper
 /// <reference path="f:\mc\lllib/Library/JS/Api.js" /> 
+
 var PluginName = "pSurvivalFly";
 var PluginIntroDuction = "生存飞行";
 var PluginVersion = [1,0,0];
@@ -7,13 +8,18 @@ var PluginOtherInformation = { "插件作者": "phj233" };
 ll.registerPlugin(PluginName, PluginIntroDuction, PluginVersion, PluginOtherInformation);
 
 var price = 60;
+ 
+// if(!File.exists(`.\\plugins\\${pluginName}\\config.json`)){
+//     var conf = new JsonConfigFile(`.\\plugins\\${pluginName}\\config.json`);
+//     conf.init("price",60);
+// }
 
 mc.listen("onServerStarted", () => {
-    let cmd = mc.newCommand("psfly", "§4pfly§3生存飞行",PermType.Any);
+    let cmd = mc.newCommand("psfly", "§6pfly§e生存飞行",PermType.Any);
     cmd.setAlias("psf");
     cmd.setEnum("ac", ["gui","mgr"]);
-    cmd.mandatory("action", ParamType.Enum, "ac");
-    cmd.overload("ac");
+    cmd.mandatory("action", ParamType.Enum, "ac",1);
+    cmd.overload(["ac"]);
     cmd.setCallback((_cmd, _ori,_out,res) => {
         switch (res.action) {
             case "gui":
@@ -22,12 +28,13 @@ mc.listen("onServerStarted", () => {
             case "mgr":
                 if(_ori.player.isOP()){
                     mgr_Gui(_ori.player);
-                    break;
                 } else{
                     _ori.player.sendToast("你妹有权限捏","你没资格啊，你没资格啊，你没资格啊");
                 }
+                break;
             default:
                 p_Gui(_ori.player);
+                break;
         }
     });
     cmd.setup();
@@ -45,9 +52,9 @@ function getolplayerarr(){
 
 function p_Gui(player){
     let playerGui = mc.newCustomForm();
-    playerGui.setTitle("§4§lpSurvivalFly");
+    playerGui.setTitle("§6§lpSurvivalFly");
     playerGui.addInput("您需要的飞行时长(分)：","","5");
-    playerGui.addLabel(`§e目前单价"${price}"金币`);
+    playerGui.addLabel(`§e目前单价-${price}-金币`);
     player.sendForm(playerGui,(player,data) => {
         if(data!=0){
             playerFly(player,data[0]);
@@ -59,13 +66,13 @@ function p_Gui(player){
 
 function mgr_Gui(player){
     let mgrGui = mc.newCustomForm();
-    onlinePlayer=getolplayerarr();
-    mgrGui.setTitle("§4§lpsFly-mgr");
+    let onlinePlayer=getolplayerarr();
+    mgrGui.setTitle("§6§lpsFly-mgr");
     mgrGui.addDropdown("选择一个玩家让他飞行",onlinePlayer);
     mgrGui.addInput("飞行时间(分)","5");
     player.sendForm(mgrGui,(player,data)=>{
         if(data!=null){
-            playerFly(player,data[1])
+            playerFly(data[0],data[1])
         } else{
 
         }
@@ -73,7 +80,14 @@ function mgr_Gui(player){
 }
 
 function playerFly(pl,time){
-
+    if(!pl.isOP()){
+        money.reduce(pl.xuid,time*60*1000);
+        
+        setTimeout((pl)=>{
+        mc.runcmd(``)
+    },time*60*1000);
+    }
 }
+    
 
 
