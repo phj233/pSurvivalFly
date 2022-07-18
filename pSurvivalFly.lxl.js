d8258fdf7121fdbb4970a11014e5c8e925bb6ac2
phj233 <2780990934@qq.com>
2022-07-18 11:50:55 +0800

    ~~

--- pSurvivalFly.lxl.js
+++ pSurvivalFly.lxl.js
@@ -14,17 +14,35 @@ mc.listen("onServerStarted", () => {
     cmd.setEnum("ac", ["gui","mgr"]);
     cmd.mandatory("action", ParamType.Enum, "ac");
     cmd.overload("ac");
-    cmd.setCallback((_cmd, _ori,_out, res) => {
+    cmd.setCallback((_cmd, _ori,_out,res) => {
         switch (res.action) {
             case "gui":
                 p_Gui(_ori.player);
-            default:
-
                 break;
+            case "mgr":
+                if(_ori.player.isOP()){
+                    mgr_Gui(_ori.player);
+                    break;
+                } else{
+                    _ori.player.sendToast("你妹有权限捏","你没资格啊，你没资格啊，你没资格啊");
+                }
+            default:
+                p_Gui(_ori.player);
         }
     });
     cmd.setup();
 });
+
+function getolplayerarr(){
+    let onlinePlayer=[];
+    let olPlayerData=mc.getOnlinePlayers();
+    j=olPlayerData.length;
+    for(let i=0;i<j;i++){
+        onlinePlayer.push(olPlayerData[i].realName);
+    }
+    return onlinePlayer;
+}
+
 function p_Gui(player){
     let playerGui = mc.newCustomForm();
     playerGui.setTitle("§4§lpSurvivalFly");
@@ -32,26 +50,22 @@ function p_Gui(player){
     playerGui.addLabel(`§e目前单价"${price}"金币`);
     player.sendForm(playerGui,(player,data) => {
         if(data!=0){
-
+            playerFly(player,data[0]);
+        } else{
+            player.tell("请输入数值",4);
         }
-
     })
 }
 
 function mgr_Gui(player){
     let mgrGui = mc.newCustomForm();
-    let onlinePlayer=[];
-    let olPlayerData=mc.getOnlinePlayers();
-    j=olPlayerData.length;
-    for(let i=0;i<j;i++){
-        onlinePlayer.push(olPlayerData[i].realName);
-    }
+    onlinePlayer=getolplayerarr();
     mgrGui.setTitle("§4§lpsFly-mgr");
     mgrGui.addDropdown("选择一个玩家让他飞行",onlinePlayer);
     mgrGui.addInput("飞行时间(分)","5");
     player.sendForm(mgrGui,(player,data)=>{
         if(data!=null){
-
+            playerFly(player,data[1])
         } else{
 
         }
